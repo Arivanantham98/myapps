f819c18c7b91d56f021230f5d9e5615fab412259
Arivanantham98 <arivanantham1003@gmail.com>
2021-10-09 17:36:13 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { HomePage } from './app/containers/HomePage';
+
+const AppContainer = styled.div`
+  ${tw`
+      w-full
+  `};
+`;
+
+function App() {
+  return  <AppContainer>
+      <HomePage/>
+   </AppContainer>
+  
+}
+
+export default App;
